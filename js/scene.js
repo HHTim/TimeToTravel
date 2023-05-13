d541d89666e5f077307e5210a7471837ffd8a7f5
LAPTOP-PATF7J21\Tibame_T14 <Nancy@gmail.com>
2023-05-13 17:54:05 +0800

    Initialize Branch

--- js/scene.js
+++ js/scene.js
@@ -1,14 +1,26 @@
+// ========= Card 收合 ==========//
+$(function () {
+  $('.toggle-text').on('click', function () {
+    var text = $(this).prev('.card-text');
+    text.toggleClass('multiline-ellipsis');
+    if (text.hasClass('multiline-ellipsis')) {
+      $(this).text('查看更多▼');
+    } else {
+      $(this).text('收起 ▲');
+    }
+  });
+});
+
 function openTab(tabName) {
-    var i, tabContent, tabLinks;
-    tabContent = document.getElementsByClassName("tab-content");
-    for (i = 0; i < tabContent.length; i++) {
-      tabContent[i].style.display = "none";
-    }
-    tabLinks = document.getElementsByClassName("tab");
-    for (i = 0; i < tabLinks.length; i++) {
-      tabLinks[i].classList.remove("active");
-    }
-    document.getElementById(tabName).style.display = "block";
-    event.currentTarget.classList.add("active");
+  var i, tabContent, tabLinks;
+  tabContent = document.getElementsByClassName('tab-content');
+  for (i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = 'none';
   }
-  
+  tabLinks = document.getElementsByClassName('tab');
+  for (i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].classList.remove('active');
+  }
+  document.getElementById(tabName).style.display = 'block';
+  event.currentTarget.classList.add('active');
+}
