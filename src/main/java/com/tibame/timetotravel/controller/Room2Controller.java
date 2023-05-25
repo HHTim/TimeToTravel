3f7dffb8eaec51d235ba54933d2dbd0d80b9a9a3
EricYeh <spu8673@gmail.com>
2023-05-25 21:38:07 +0800

    fix: remove HttpServletResponse

--- src/main/java/com/tibame/timetotravel/controller/Room2Controller.java
+++ src/main/java/com/tibame/timetotravel/controller/Room2Controller.java
@@ -2,7 +2,6 @@ package com.tibame.timetotravel.controller;
 
 import com.tibame.timetotravel.service.Room2Service;
 import com.tibame.timetotravel.view.ViewCompanyRoom;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,14 +22,8 @@ public class Room2Controller {
             @PathVariable String keyWord,
             @PathVariable Integer people,
             @PathVariable String start,
-            @PathVariable String end,
-            HttpServletResponse resp) {
-
-        resp.setHeader("Access-Control-Allow-Origin", "*"); // 允許來自所有網域的請求
-        resp.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // 允許的 HTTP 方法
-        resp.setHeader("Access-Control-Allow-Headers", "Content-Type"); // 允許的請求頭
-        resp.setHeader("Access-Control-Allow-Credentials", "true"); // 是否允許帶有憑證的請求
-
+            @PathVariable String end
+    ) {
         System.out.println("收到請求");
         List<ViewCompanyRoom> availableCompanies = room2Service.findAvailableCompany(keyWord, people, start, end);
         return availableCompanies;
