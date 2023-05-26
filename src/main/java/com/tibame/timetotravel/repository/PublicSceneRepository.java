bc2efd0b9b40b7b46f848e6f78ea6aca99c8813f
EricYeh <spu8673@gmail.com>
2023-05-26 19:58:20 +0800

    fix: add format

--- src/main/java/com/tibame/timetotravel/repository/PublicSceneRepository.java
+++ src/main/java/com/tibame/timetotravel/repository/PublicSceneRepository.java
@@ -1,9 +1,9 @@
 package com.tibame.timetotravel.repository;
 
-import com.tibame.timetotravel.Entity.PublicScene;
+import com.tibame.timetotravel.entity.PublicScene;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository("PublicSceneRepository")
-public interface PublicSceneRepository extends JpaRepository<PublicScene,Integer> {
+public interface PublicSceneRepository extends JpaRepository<PublicScene, Integer> {
 }
