2055a3ddffc4e48beccfd2618aea926e683050ca
Lyon <andy592490@gmail.com>
2023-06-06 22:58:01 +0800

    gift order management 70%

--- src/main/java/com/tibame/timetotravel/Service/GiftOrderManageService.java
+++ src/main/java/com/tibame/timetotravel/Service/GiftOrderManageService.java
@@ -8,16 +8,12 @@ import java.util.List;
 public interface GiftOrderManageService {
 
     //    basic 功能
-    ViewGiftOrderManage findById(Integer giftId);
+//    ViewGiftOrderManage findById(Integer giftId);
 
     List<ViewGiftOrderManage> findAll();
 
     //搜尋
-    List<ViewGiftOrderManage> findByGiftOrder(Integer giftOrderId, Integer userId);
-
-
-
+//    List<ViewGiftOrderManage> findByGiftOrder(Integer giftOrderId, Integer userId);
     List<ViewGiftOrderManage> findByGiftOrderId(Integer giftOrderId);
-
-    List<ViewGiftOrderManage> findByUserId(Integer userId);
+    List<ViewGiftOrderManage> findByUserAccount(Integer userAccount);
 }
